0da4104cd7fff0a098c74875394fc8414ef6fd55
MrDoctorKovacic <qcasey1@gmail.com>
2019-08-30 16:07:05 -0700

    don't need filename

--- alpr.go
+++ alpr.go
@@ -3,19 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 )
 
 func alprImage(filename string) {
 	log.Println("running ALPR on ", filename)
 
-	_, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out, err := exec.Command(fmt.Sprintf("/usr/bin/alpr -j -c us h786poj.jpg")).Output()
+	out, err := exec.Command(fmt.Sprintf("/usr/bin/alpr -j -c us %s", filename)).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
