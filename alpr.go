71d40e47a53e25f8d07fdc0c8b1557191d23ab14
MrDoctorKovacic <qcasey1@gmail.com>
2019-08-30 16:08:15 -0700

    returning pwd

--- alpr.go
+++ alpr.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func alprImage(filename string) {
 	log.Println("running ALPR on ", filename)
 
-	out, err := exec.Command(fmt.Sprintf("/usr/bin/alpr -j -c us %s", filename)).Output()
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := exec.Command(fmt.Sprintf("/usr/bin/alpr -j -c us %s/%s", dir, filename)).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
